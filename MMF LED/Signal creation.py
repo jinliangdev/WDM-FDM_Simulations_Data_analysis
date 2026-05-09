fde99c7f6c64bd48e9195883d31749fa26660939
Ying Jin Liang <164493244+jinliangdev@users.noreply.github.com>
2026-05-09 14:17:21 +0100

    test

---
+++ MMF LED/Signal creation.py
@@ -0,0 +1,12 @@
+"""
+
+WDM in a MMF
+
+Single LED channel, step-index MM
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from scipy.special import erfc
+
